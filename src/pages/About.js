d79b0fff60342856f7ac21503f80a65e253306ce
ValletteY <vallette.y@gmail.com>
2021-09-24 18:29:40 +0200

    Version navigation&style

---
+++ src/pages/About.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+const About = () => {
+    return (
+        <div>
+            <Navigation />
+            <h1> A propos</h1>
+            <br/>
+             <p>
+                 lorem ipsum dolorllum, consectetur adipis lorem ipsum d
+             </p>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/pages/Home.js
@@ -0,0 +1,15 @@
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+return (
+    <div className="Home">
+    <Navigation />
+
+    <h1>Bonjour à vous</h1>
+    
+
+    </div>
+);
+};
+
+export default Home;
